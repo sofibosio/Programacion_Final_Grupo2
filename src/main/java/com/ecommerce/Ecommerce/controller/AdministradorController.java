3aedf2d422875b745b900a0b0d404c28f585e448
Sofia <sofiabosio5@gmail.com>
2022-10-24 15:24:21 -0300

    Se creo la vista y el controlador

--- src/main/java/com/ecommerce/Ecommerce/controller/AdministradorController.java
+++ src/main/java/com/ecommerce/Ecommerce/controller/AdministradorController.java
@@ -1,5 +1,6 @@
 package com.ecommerce.Ecommerce.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 @RequestMapping("/administrador")
 public class AdministradorController {
+
 
     @GetMapping("")
     public String home(){
