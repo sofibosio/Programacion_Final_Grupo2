a456cb25722167a90bd3e2a72250f32f05d5b276
Sofia <sofiabosio5@gmail.com>
2022-10-25 20:08:08 -0300

    Se creo el controller de producto y la vista show

--- src/main/java/com/ecommerce/Ecommerce/ECommerceApplication.java
+++ src/main/java/com/ecommerce/Ecommerce/ECommerceApplication.java
@@ -2,13 +2,18 @@ package com.ecommerce.Ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+
+
+@SpringBootApplication
+@EnableJpaRepositories
 public class ECommerceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ECommerceApplication.class, args);
 	}
+
+
 
 }
--- src/main/java/com/ecommerce/Ecommerce/service/ProductoService.java
+++ src/main/java/com/ecommerce/Ecommerce/service/ProductoService.java
@@ -1,12 +1,16 @@
 package com.ecommerce.Ecommerce.service;
 
+import java.util.List;
 import java.util.Optional;
 
-import com.ecommerce.Ecommerce.model.Producto;
+import org.springframework.stereotype.Service;
 
+import com.ecommerce.Ecommerce.model.Producto;
+@Service
 public interface ProductoService {
-    public Producto save(Producto producto);
-    public Optional<Producto> get(Integer id);
-    public void update(Producto producto);
-    public void delete(Integer id);
+	public Producto save( Producto producto);
+	public Optional<Producto> get(Integer id);
+	public void update(Producto producto);
+	public void delete(Integer id);
+
 }
